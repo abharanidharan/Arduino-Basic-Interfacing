294942db884a1fd87b8221a5c11bc9baac77e0df
abharanidharan <fossclub@srec.ac.in>
2016-02-26 17:35:45 +0530

    Added files via upload

---
+++ bluetooth.ino
@@ -0,0 +1,35 @@
+     int ledpin=13;
+int state;
+int flag=0;            
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledpin,OUTPUT);
+}
+void loop()
+{
+  if(Serial.available()>0)
+  {
+    state=Serial.read();
+    flag=0;
+  }
+  if(state=='1')
+  {
+    digitalWrite(ledpin,HIGH);
+    if(flag==0)
+    {
+      Serial.println("LED is on");
+      flag=1;
+    }
+  }
+    if(state=='0')
+    {
+      digitalWrite(ledpin,LOW);
+      if(flag==0)
+      {
+        Serial.println("LED is off");
+        flag=0;
+      }
+    }
+  
+}
